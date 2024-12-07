dcb4ef098f995db0bac3332a0e08c2c1a832a635
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-12-08 00:25:15 +0200

    improve hatch

---
+++ pyaec/build.py
@@ -0,0 +1,30 @@
+from typing import Any
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+import os
+
+
+class SpecialBuildHook(BuildHookInterface):
+    PLUGIN_NAME = "platform specific"
+
+    def __init__(self, *args):
+        print(__file__)
+        print(os.getcwd())
+        print(args)
+        super().__init__(*args)
+
+    def initialize(self, version, build_data):
+        # Wheel is platform specific!
+        build_data["pure_python"] = False
+
+        # Create platform specific named wheel!
+        # https://docs.python.org/3/library/sysconfig.html#sysconfig.get_platform
+        # https://peps.python.org/pep-0425/
+        # py3-none-win-amd64
+        # py3-none-linux_x86_64
+        # py3-none-macosx_14_0_x86_64
+        env_wheel_tag = os.getenv("WHEEL_TAG")
+        if env_wheel_tag:
+            build_data["tag"] = env_wheel_tag
+        else:
+            # Strict tag to OS and Python version
+            build_data["infer_tag"] = True
